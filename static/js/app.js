6446cad3d620d3bd836002b1e21e102a71a42e58
peatk <peterklinecp@gmail.com>
2024-01-31 13:29:30 -0800

    got some stuff working

--- static/js/app.js
+++ static/js/app.js
@@ -3,17 +3,31 @@ const bellyURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-class
 
 // Load data 
 d3.json(bellyURL).then(function(data) {
-    let meta = data['metadata'];
-    // let first_meta = meta[0];
+// d3.json('samples.json').then(function(data) {
+    let meta = data.metadata;
+    console.log('this is the metadata:');
+    console.log(meta);
     
-    let all_samps = data['samples']
-    // let first_samp = all_samps[1];
-    // console.log(meta)
+    // let all_samps = data.samples[0].sample_values;
+    // console.log('these are the sample values:');
+    // console.log(all_samps);
+    
+    let all_samps = data.samples[0].sample_values.slice(0,10);
+    console.log('these are the sample values:');
+    console.log(all_samps);
 
+
+    let otu_id = data.samples[0].otu_ids.slice(0,10);
+    console.log('these are the otu ids values:');
+    console.log(otu_id)
+
+    let otu_label = data.samples[0].otu_labels.slice(0,10);
+    console.log('these are the otu labels:');
+    console.log(otu_label)
 
     let trace1 = {
-        x: meta.map(x => x.ethnicity),
-        y: all_samps.map( x => x.sample_values),
+        x: otu_id.map(x => x),
+        y: all_samps.map( x => x),
         type: 'bar'
     };
 
@@ -22,10 +36,18 @@ d3.json(bellyURL).then(function(data) {
         title: 'test'
     };
     Plotly.newPlot('plot', data1, layout);
+
+    // // On change to the DOM, call getData()
+    // d3.selectAll("#selDataset").on("change", getData);
+
+    // function getData() {
+    //     let dropdownMenu = d3.select("#selDataset");
+    //     // Assign the value of the dropdown menu option to a letiable
+    //         let dataset = dropdownMenu.property("value");
+    // };
 });
 
-// Populate dropdown menu
-//   let dropdown = d3.select("#selDataset");
 
-// let meta = data['metadata'];
-// let first_meta = meta[0];
+
+
+
