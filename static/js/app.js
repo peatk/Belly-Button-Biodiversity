c8b4194f47f0c55e819dfcd5781a6cfc18e086af
peatk <peterklinecp@gmail.com>
2024-02-04 14:13:01 -0800

    bubble working properly. working on metadata

--- static/js/app.js
+++ static/js/app.js
@@ -4,35 +4,36 @@ let bellyURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classro
 
 // have the plot update based on the selected id (dataset)
 function updateCharts(data, id) {
-    // let dropdownMenu = d3.select('#selDataset');
-    console.log(data);
-    // // value assigning for the dropdown menu to the dataset variable
-    // let dataset = dropdownMenu.property('value');
+    // console.log('this is the plot update data');
+    // console.log(data);
     let selected = data.samples.find(set => set.id == id);
+    // console.log('this is the new selected data');
+    // console.log(selected);
 
+    // bar chart updates
     let selectedSampleValues = selected.sample_values.slice(0,10).sort((a, b) => a-b);
-    console.log(selectedSampleValues);
+    // console.log(selectedSampleValues);
     let selectedOtuId = selected.otu_ids.slice(0,10);
-
     let selectedOtuIdLabel = selectedOtuId.map(id => `OTU ${id}`);
-    console.log(selectedOtuIdLabel);
-    // let selectedOtuLabel = selected.otu_labels.slice(0,10);
+    // console.log(selectedOtuIdLabel);
 
-    // let update = {
-        // x: [selectedSampleValues],
-        // y: [selectedOtuIdLabel],
-    // };
-    // Plotly.restyle('bar', update);
-
-    
     Plotly.restyle('bar', 'x', [selectedSampleValues]);
     Plotly.restyle('bar', 'y', [selectedOtuIdLabel]);
 
-    // Plotly.restyle('bubble', 'x', [selectedSampleValues]);
-    // Plotly.restyle('bubble', 'y', [selectedOtuIdLabel]);
+    // bubble chart updates
+    let bubbleSampleValues = selected.sample_values;
+    let bubbleOtuAll = selected.otu_ids;
+
+    Plotly.restyle('bubble', 'x', [bubbleOtuAll]);
+    Plotly.restyle('bubble', 'y', [bubbleSampleValues]);
+    // Plotly.restyle('bubble', 'size', [sampleValues]);
 };
 
-// function updateDemo(data, id) {
+// function updateDemo(data) {
+//     d3.json(bellyURL).then(function(data) {
+//         let metaData = data.metaData;
+//         let metaDatafilter = metaData.filter(set => set.id == id)
+// })};
 
 // }
 
@@ -45,27 +46,51 @@ d3.json(bellyURL).then(function(data) {
     });    
     
     // change the variables so that i am not drilling down on the data too early
-    let all_samps = data.samples[0].sample_values.slice(0,10).sort((a, b) => a - b);
-    console.log(all_samps);
+    
+    let dataSamples = data.samples;
+    console.log('these is data samples')
+    console.log(dataSamples);
 
-    let otu_id = data.samples[0].otu_ids.slice(0,10);
-    let id = otu_id.map(id => `OTU ${id}`);
-    console.log(id);
+    let sampleValues = dataSamples[0].sample_values;
+    console.log('these are the sample values');
+    console.log(sampleValues);
 
-    let otu_label = data.samples[0].otu_labels.slice(0,10);
-    console.log('these are the otu labels:');
-    console.log(otu_label);
+    let sampleValuessorted = dataSamples[0].sample_values.slice(0,10).sort((a, b) => a - b);
+    console.log('these is sliced and sorted data samples')
+    console.log(sampleValuessorted);
 
-    let trace1 = {
-        y: id,
-        x: all_samps,
+    let otuAll = dataSamples[0].otu_ids;
+    console.log('these is all otu ids')
+    console.log(otuAll);
+
+    let otuSliced = dataSamples[0].otu_ids.slice(0,10);
+    let otuId = otuSliced.map(id => `OTU ${id}`);
+    console.log(otuId);
+
+    let otuLabelall = dataSamples[0].otu_labels;
+    console.log('these is all otu labels');
+    console.log(otuLabelall);
+
+    let otuLabelsliced = dataSamples[0].otu_labels.slice(0,10);
+    console.log('these are the slcied otu labels:');
+    console.log(otuLabelsliced);
+
+    // let metaData = data.metadata.find(set => set.id == id);
+    // let metaDataname = metaData.name;
+    // console.log('these are the metadata:');
+    // console.log(metaData);
+    // console.log(metaDataname);
+
+
+    let barCharttrace = {
+        y: otuId,
+        x: sampleValuessorted,
         type: 'bar',
         orientation: 'h'
     
     };
-
-    let data1 = [trace1]
-    let layout1 = {
+    let barChartdata = [barCharttrace]
+    let barChartlayout = {
         title: 'bar',
         margin: {
             l: 100,
@@ -74,45 +99,30 @@ d3.json(bellyURL).then(function(data) {
             b: 100
         }
     };
-    // let bubbleId = firstSample.otu_ids;
-    // let bubbleLabel = firstSample.otu_labels;
-    // // let bubbleValue = firstSample.sample_values;
-    // console.log('this is the bubble data');
-    // console.log(bubbleId);
-    // console.log(bubbleLabel);
-
-    // let bubbleTrace = [{
-    //     x: 
-    //     y: 
-    //     text: 
-    //     mode: 'markers',
-    //     type: 'bubble',
-    //     marker: {
-    //         size: ,
-    //         color: ,
-    //         colorscale: 'turbo'
-    //     }
-    //     // orientation: 'h'
-    
-    // }];
-    
-    // let bubbleData = []
-    // let bubbleLayout = {
-    //     title: 'bubble',
-    //     showlegend: false,
-    //     // xaxis: {title: `OTU ID`, automargin: true},
-    //     // yaxis: {automargin: true},
-    //     // hovermode: 'closest'
-    //     margin: {
-    //         l: 100,
-    //         r: 100,
-    //         t: 100,
-    //         b: 100
-    //     }
+    let bubbleChartdata =[{
+        x: otuAll,
+        y: sampleValues,
+        // r: sampleValues,
+        mode: 'markers',
+        marker: {
+            size: sampleValues,
+            color: otuAll,
+            // colorscale: 'plasma'
+            // colorscale: 'yellow'
+        }
+    }];
+    let bubbleChartlayout = {
+        title: 'bubble test',
+        xaxis: {title: 'x axis'},
+        yaxis: {title: 'y axis'}
+    };
+    // let metadataChart ={
+    //     name: metaData.name
     // };
 
-    Plotly.newPlot('bar', data1, layout1);
-    // Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+    Plotly.newPlot('bar', barChartdata, barChartlayout);
+    Plotly.newPlot('bubble', bubbleChartdata, bubbleChartlayout);
+    // Plotly.newPlot('sample-metadata', metadataChart);
         // Set up event listener for changes in the dropdown menu
         d3.selectAll("#selDataset").on("change", function (event) {
             console.log('selectChangeEvent', event);
